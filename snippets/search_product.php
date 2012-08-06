45a068cd149e3f0bcf9495f4d24bb2634f8ed195
andreas <andreaswijaya89@yahoo.com>
2012-08-06 16:55:05 +0700

    update search product

--- snippets/search_product.php
+++ snippets/search_product.php
@@ -5,15 +5,25 @@ $api_server = "http://sg2.facetly.com/1";
 $api_path = "search/product";
 $api_method = "GET";
 $api_data = array(
- "key" => "zuakz7ok",
- "limit" => 3,
- "searchtype" => 'html',
- "baseurl" => '/facetly/search-ajax.php',
- "query" => "acer",
+ 		  "key" => "ider3nks",
+		  "limit" => 3,
+		  "searchtype" => 'html',
+		  "baseurl" => $_SERVER['SCRIPT_NAME'],
+		  "query" => "acer",
 );
- 
+$api_filter = $_GET;
+$api_data = array_merge($api_data,$api_filter); 
 $facetly->setServer($api_server);
 $api_output = $facetly->call($api_path, $api_data, $api_method);
 $return = json_decode($api_output);
- 
-print_r($return); 
+if(!isset($return)) {
+  echo "Product not found";
+}
+else
+{ 
+  echo "<div id='facetly_facet'>"; 
+  print_r($return->facets); 
+  echo "</div><div id='facetly_result'>";
+  print_r($return->results);
+  echo "</div>";
+}
